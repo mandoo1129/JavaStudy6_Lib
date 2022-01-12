2e5cd4ce5d0ec0da934053b01c18a115e2b27b47
mandoo1129 <leeminjoo02011@gmail.com>
2022-01-12 17:40:06 +0900

    Student Data update

---
+++ src/com/iu/s3/ex2/StudentData.java
@@ -0,0 +1,18 @@
+package com.iu.s3.ex2;
+
+public class StudentData {
+	
+	private String info;
+	
+	public StudentData() {
+		this.info = "name1,1,15,50,60,";
+		this.info = this.info + "name2,2,52,68,40,";
+		this.info = this.info + "name3,3,8,87,75,";
+	}
+	
+	//학생들의 정보를 파싱해서 총점과 평균도 계산 후 전체 출력
+	//학생의 정보를 추가
+	//학생의 정보를 삭제
+	
+	
+}
