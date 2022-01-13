443351886a1daf4c23a4d85e156f4784d2149dcd
mandoo1129 <leeminjoo02011@gmail.com>
2022-01-13 12:14:19 +0900

    StudentDTO update

--- src/com/iu/s3/ex1/member/StudentData.java
+++
@@ -1,5 +0,0 @@
-package com.iu.s3.ex1.member;
-
-public class StudentData {
-
-}
---
+++ src/com/iu/s3/ex1/student/StudentData.java
@@ -0,0 +1,18 @@
+package com.iu.s3.ex1.student;
+
+import java.util.List;
+import java.util.Map;
+
+import com.iu.s3.ex1.member.MemberDTO;
+
+public class StudentData {
+
+	public Map<String, StudentDTO> addStudent(List<MemberDTO> ar){
+		
+		
+		return null;
+		
+	}
+	
+}
+ 
